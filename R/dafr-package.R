319cc373972ce9e74d0446990fffc755be0d6d3e
Aviezer Lifshitz <aviezer.lifshitz@weizmann.ac.il>
2025-04-30 15:22:52 +0300

    Update DESCRIPTION and NAMESPACE to import 'methods' package functions

--- R/dafr-package.R
+++ R/dafr-package.R
@@ -4,6 +4,7 @@
 ## usethis namespace: start
 #' @importFrom JuliaCall julia_call
 #' @importFrom JuliaCall julia_eval
+#' @importFrom methods as
 #' @importFrom tibble as_tibble
 #' @importFrom tibble has_name
 ## usethis namespace: end
