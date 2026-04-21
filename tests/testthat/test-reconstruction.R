9f9f33c2a6333f4a8e408e65acd26b542a68fd15
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2026-04-21 18:52:45 +0300

    test(reconstruction): empty_implicit / skipped_properties / implicit_properties

--- tests/testthat/test-reconstruction.R
+++ tests/testthat/test-reconstruction.R
@@ -17,3 +17,42 @@ test_that("reconstruct_axis creates new axis from unique implicit values", {
     expect_true(has_vector(d, "cell", "single_score"))
     expect_identical(empties$donor_age, NULL)
 })
+
+test_that("reconstruct_axis: empty_implicit marks entries as no-value", {
+    d <- memory_daf(name = "d")
+    add_axis(d, "cell", c("c1", "c2", "c3", "c4"))
+    set_vector(d, "cell", "type", c("T", "NA", "B", "NA"))
+    set_vector(d, "cell", "color", c("red", "gray", "blue", "gray"))
+
+    empties <- reconstruct_axis(d,
+        existing_axis = "cell", implicit_axis = "type",
+        empty_implicit = "NA")
+
+    expect_identical(axis_vector(d, "type"), c("B", "T"))
+    expect_identical(unname(get_vector(d, "type", "color")),
+                     c("blue", "red"))
+    expect_identical(empties$color, "gray")
+})
+
+test_that("reconstruct_axis: skipped_properties excludes from migration", {
+    d <- memory_daf(name = "d")
+    add_axis(d, "cell", c("c1", "c2"))
+    set_vector(d, "cell", "donor", c("dA", "dB"))
+    set_vector(d, "cell", "age", c(30L, 40L))
+    reconstruct_axis(d, existing_axis = "cell", implicit_axis = "donor",
+                     skipped_properties = "age")
+    expect_true(has_vector(d, "cell", "age"))
+    expect_false(has_vector(d, "donor", "age"))
+})
+
+test_that("reconstruct_axis: implicit_properties enforces consistency", {
+    d <- memory_daf(name = "d")
+    add_axis(d, "cell", c("c1", "c2", "c3"))
+    set_vector(d, "cell", "donor", c("dA", "dA", "dB"))
+    set_vector(d, "cell", "tag", c("x", "y", "z"))
+    expect_error(
+        reconstruct_axis(d, existing_axis = "cell", implicit_axis = "donor",
+                         implicit_properties = "tag"),
+        "inconsistent"
+    )
+})
